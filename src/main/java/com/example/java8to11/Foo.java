942e8a1157fe6bdce2ff1cce2c582aa38b656427
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-11-15 15:12:25 +0900

    자바에서 제공하는 함수형 인터페이스

--- src/main/java/com/example/java8to11/Foo.java
+++ src/main/java/com/example/java8to11/Foo.java
@@ -1,20 +1,32 @@
 package com.example.java8to11;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public class Foo {
     public static void main(String[] args) {
 
-        // 익명 내부 클래스 anonymous inner Clss
-/*
-        RunSomething runSomething = new RunSomething() {
-            @Override
-            public void doIt() {
-                System.out.println("Hello");
-            }
-        };
-*/
-        RunSomething runSomething = n -> n + 10;
+        System.out.println(new Plus10().apply(10));
+        Function<Integer, Integer> plus10 = n -> n + 10;
+        Function<Integer, Integer> multiply2 = n -> n * 2;
 
-        System.out.println(runSomething.doIt(10));
+        System.out.println(plus10.apply(10));
+
+        System.out.println(plus10.compose(multiply2).apply(10));
+
+        System.out.println(plus10.andThen(multiply2).apply(3));
+
+        Consumer<Integer> printT = i -> System.out.println(i);
+        printT.accept(10);
+
+        Supplier<Integer> get10 = () -> 10;
+
+        System.out.println(get10);
+
+        Predicate<String>startsWithSeokmun = s -> s.startsWith("Seokmun");
+        System.out.println(startsWithSeokmun.test("Seokmun"));
 
     }
 
