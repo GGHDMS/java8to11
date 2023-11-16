53acb9d4c90571e81cb78c071f755205fe399c50
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-11-16 17:08:59 +0900

    자바 8 API의 기본 메소드와 스태틱 메소드

--- src/main/java/com/example/java8to11/section2/App.java
+++ src/main/java/com/example/java8to11/section2/App.java
@@ -1,12 +1,50 @@
 package com.example.java8to11.section2;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+
 public class App {
 
     public static void main(String[] args) {
-        Foo foo = new DefaultFoo("seokmun");
+        ArrayList<String> name = new ArrayList<>();
 
-        foo.printName();
-        foo.printNameUpperCase();
-        Foo.printAnything();
+        name.add("seokmun");
+        name.add("whiteship");
+        name.add("toby");
+        name.add("foo");
+
+        name.forEach(System.out::println);
+
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator2 = spliterator.trySplit();
+
+        while (spliterator.tryAdvance(System.out::println)) ;
+        System.out.println("============================");
+        while (spliterator2.tryAdvance(System.out::println)) ;
+
+        long s1 = name.stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("S"))
+                .count();
+        System.out.println(s1);
+
+        Set<String> s2 = name.stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("S"))
+                .collect(Collectors.toSet());
+
+        name.removeIf(s -> s.startsWith("s"));
+
+        System.out.println("======================");
+        name.forEach(System.out::println);
+
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        name.sort(compareToIgnoreCase.reversed().thenComparing(compareToIgnoreCase));
+
+
+        name.forEach(System.out::println);
     }
 }
